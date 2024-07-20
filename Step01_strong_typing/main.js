e974b48e7699538957044109aafa063d194d7675
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-07-20 16:14:44 -0700

    Add files via upload

---
+++ Step01_strong_typing/main.js
@@ -0,0 +1,17 @@
+//                                        STEP 01
+//                                      STRONG TYPING
+/*
+Typescript enhance javascript by adding a type system. This means you can specify what type of data
+(such as string , number, boolean or any complex object) can be stored in a variable . This specification
+helps catch error during developement, making your code  more robust and maintainable  */
+var userName = "Aleeza";
+var userAge = 17;
+var isStudent = true;
+//                                     T YPE INFERENCE
+// Type inference in TypeScript allows the compiler to automatically determine the type of a variable
+//  based on the value assigned to it. This means you don't always have to explicitly declare the type.
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+g = true;
---
+++ Step01_strong_typing/main.ts
@@ -0,0 +1,20 @@
+//                                        STEP 01
+//                                      STRONG TYPING
+/*
+Typescript enhance javascript by adding a type system. This means you can specify what type of data
+(such as string , number, boolean or any complex object) can be stored in a variable . This specification
+helps catch error during developement, making your code  more robust and maintainable  */
+
+let userName : string = "Aleeza";
+let userAge : number = 17;
+let isStudent : boolean = true;
+
+//                                     T YPE INFERENCE
+// Type inference in TypeScript allows the compiler to automatically determine the type of a variable
+//  based on the value assigned to it. This means you don't always have to explicitly declare the type.
+
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
