4e398df6146c7258dd61c94da8bd0c3e600d96a6
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-07-22 14:29:21 -0700

    Update third.js

--- Step03(a)(b)/third.js
+++ Step03(a)(b)/third.js
@@ -1,7 +1,7 @@
 import greet from "./first.js";
 greet();
-import { names } from "./first.js";
-console.log(names);
+import { names as usernames } from "./first.js";
+console.log(usernames);
 import { add } from "./second.js";
 console.log(add(10, 7));
 import { info1, info2, info3 } from "./second.js";
