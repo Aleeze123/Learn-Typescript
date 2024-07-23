dae6c8bc8ab0ccde2c9b79c35978d8e48044d732
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-07-23 12:09:52 -0700

    Add files via upload

---
+++ Step6_explict_casting/main.ts
@@ -0,0 +1,10 @@
+//                                      Step 06
+//                                   Explict Casting
+// let myAge :unknown = 123
+// console.log((myAge as number).toFixed(2));
+// console.log((<number> myAge).toFixed(2));
+
+
+// let myName = "Aleeza";
+// console.log((myName as string).length);
+// console.log((<string> myName).length);
