2a4e9843c5a3a6bef772427acfbd24357ed3027a
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-07-23 04:32:02 -0700

    Add files via upload

---
+++ step04- unions-literals/main.ts
@@ -0,0 +1,46 @@
+
+// Difference b/w union / literal types
+
+//union types
+// let userinfo :string | number 
+// userinfo = "Aleeza"
+// userinfo =123
+// userinfo = true//error bcz type boolean not assignable in type string |number
+// userinfo =undefined //error bcz undefined not assignable to type string |number
+
+
+// //literal types
+// let userinfo : "Aleeza" | 123;
+// userinfo= 123
+// userinfo = true //error Type 'true' is not assignable to type '"Aleeza" | 123'.
+// userinfo= 1710 //error
+
+
+//narrowing
+// let userId : string | number |null;
+// userId = "aiaiai"
+// console.log(userId.toUpperCase());
+
+//dynamic
+// let user = Math.random()>0.5 ?"Aleeza" : 1710.00
+// if(typeof user === "string"){
+//     console.log(user.toUpperCase());
+// }
+// else{
+//     console.log(user.toFixed());
+    
+// }
+// console.log(user);
+
+
+// // You can also define a type alias
+// type RawData = boolean | number | string | null | undefined;
+
+// let data: RawData;
+// You can even combine them
+
+
+
+
+
+
